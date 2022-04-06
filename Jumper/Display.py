4e53ba60be97feed78ee0559f101902e4e130b6f
bdcrisco <bdcrisco98@gmail.com>
2022-04-05 20:17:57 -0600

    Completed the word class, minor changes to display

--- Jumper/Display.py
+++ Jumper/Display.py
@@ -1,11 +1,12 @@
 class Display:
     def __init__(self):
-        pass
+        self.state = True
 
     def print_game(self, clue, jumper):
         # Print the current game state, including the ground
         print(f"{clue}\n")
         print(f"{jumper}\n")
+        self._print_ground()
     
     def _print_ground(self):
         print("^^ ^^ ^ ^")
--- Jumper/Word.py
+++ Jumper/Word.py
@@ -1,25 +1,40 @@
-import random
-
+from random import choice as randchoice
 class Word:
     def __init__(self, filename = "words.txt"):
         self._word = self._read_file(filename)
         self._clue = []
-        for _ in self._word:
-            self._clue.append("_")
+        for letter in self._word:
+            if letter == " ":
+                self._clue.append(" ")
+            else:
+                self._clue.append("_")
 
     def _read_file(self, filename):
-        # Read the file and pick a random word
-        # Overwrite _word
-        return ""
+        # Read the file and return a random word
+        words = []
+        with open(filename) as f:
+            for line in f:
+                words.append(line.strip('\n'))
+        return randchoice(words)
 
-    def _update_clue(self):
+    def _update_clue(self, letter, i):
         # Replace the clue's underscores with the revealed letters
-        pass
+        self._clue[i] = letter
 
     def check_letter(self, letter):
         # Return the existance of a letter in the word
-        pass
+        flag = False
+        for i in range(len(self._word)):
+            if self._word[i].lower() == letter.lower():
+                self._update_clue(self._word[i], i)
+                flag = True
+
+        return flag
 
     def get_clue(self):
         # Return the decyphered word
-        pass
+        return ''.join(self._clue)
+
+    def is_match(self):
+        # returns whether the clue matches the word or not
+        return self._word == self.get_clue()
